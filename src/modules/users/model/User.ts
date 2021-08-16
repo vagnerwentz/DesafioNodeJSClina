33bc6730151a15b6d7aac2bfe002900a549f1c87
Vagner Wentz <vagner.wentz@objective.com.br>
2021-08-16 17:53:11 -0300

    feat: Change the model to be able to use TypeORM.

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,23 +1,27 @@
-import { v4 as uuidV4 } from 'uuid';
+import { Column, Entity, CreateDateColumn, UpdateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('users')
 class User {
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @Column()
     name: string;
    
+    @Column()
     email: string;
 
+    @Column()
+    password: string;
+
+    @Column()
     avatar: string;
 
+    @CreateDateColumn()
     created_at: Date;
 
+    @UpdateDateColumn()
     updated_at: Date;
-
-    constructor() {
-        if (!this.id) {
-            this.id = uuidV4();
-        }
-    }
 }
 
 export { User };
