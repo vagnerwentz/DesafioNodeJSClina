5fe2dbf39ec6004e2a1d832722ad1ccfdb529629
Vagner Wentz <vagner.wentz@objective.com.br>
2021-08-16 12:32:01 -0300

    test: Ensure the repository it is able to create a new user.

---
+++ src/__tests__/users/repositories/UsersRepository.spec.ts
@@ -0,0 +1,28 @@
+import { validate } from "uuid";
+import { UsersRepository } from "../../../modules/users/repositories/implementations/UsersRepository";
+
+describe("Users Repository", () => {
+    let usersRepository: UsersRepository;
+
+    beforeAll(() => {
+        usersRepository = UsersRepository.getInstance();
+    });
+
+    it("should be able to create a new user", () => {
+        const user = usersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@email.com',
+            avatar: 'johndoeavatar',
+        });
+
+        expect(user).toMatchObject({
+            name: 'John Doe',
+            email: 'johndoe@email.com',
+            avatar: 'johndoeavatar',
+        });
+
+        expect(validate(user.id)).toBe(true);
+        expect(user.created_at).toBeInstanceOf(Date);
+        expect(user.updated_at).toBeInstanceOf(Date);
+    });
+});
