05db2cb8d08c8d42746fac228db38af94138bc67
Vagner Wentz <vagner.wentz@objective.com.br>
2021-08-16 12:17:20 -0300

    [CLN-0001] Feat - Ensure to create a user with all props.

---
+++ src/modules/users/model/User.ts
@@ -0,0 +1,23 @@
+import { v4 as uuidV4 } from 'uuid';
+
+class User {
+    id: string;
+
+    name: string;
+   
+    email: string;
+
+    avatar: string;
+
+    created_at: Date;
+
+    updated_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+        }
+    }
+}
+
+export { User };
