459502ba17148f8849abeb86d5cc33e411af8f22
Vagner Wentz <wentz.vagner@gmail.com>
2021-08-16 23:57:51 -0300

    fix: Adjust the user controller.

--- src/modules/users/controllers/createUserController/CreateUserController.ts
+++ src/modules/users/controllers/createUserController/CreateUserController.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from 'express';
 
-import { CreateUserUseCase } from "../useCases/CreateUserUseCase";
+import { CreateUserUseCase } from "../../useCases/CreateUserUseCase";
 
 class CreateUserController {
-    constructor(private createUserUseCase: CreateUserUseCase) {}
+    constructor(private createUserUseCase: CreateUserUseCase) { }
 
     async handle(request: Request, response: Response): Promise<Response> {
         try {
-            const { name, email, password, avatar } = request.body;
+            const { name, email, password } = request.body;
 
-            const user = await this.createUserUseCase.execute({ name, email, password, avatar });
+            const user = await this.createUserUseCase.execute({ name, email, password });
 
             return response.status(200).json({ user });
         } catch (error) {
