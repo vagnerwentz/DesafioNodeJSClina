f80ef8dd5484b79fa104b5b4803d2edf87f0b2d1
Vagner Wentz <wentz.vagner@gmail.com>
2021-08-16 23:58:52 -0300

    fix: Change the folder name - old was model - new is entities.

--- src/modules/users/entities/User.ts
+++ src/modules/users/entities/User.ts
@@ -7,15 +7,12 @@ class User {
 
     @Column()
     name: string;
-   
+
     @Column()
     email: string;
 
     @Column()
     password: string;
-
-    @Column()
-    avatar: string;
 
     @CreateDateColumn()
     created_at: Date;
