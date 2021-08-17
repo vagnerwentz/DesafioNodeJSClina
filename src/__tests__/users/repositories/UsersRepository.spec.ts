d2fe125bd70fb029eced6933202b8a3df95508c4
Vagner Wentz <vagner.wentz@objective.com.br>
2021-08-17 17:49:22 -0300

    test: Ensure the repository it is able to create, list  all users.

--- src/__tests__/users/repositories/UsersRepository.spec.ts
+++ src/__tests__/users/repositories/UsersRepository.spec.ts
@@ -1,44 +1,82 @@
-import { User } from 'modules/users/entities/User';
-import { Connection, createConnection, getRepository, Repository } from 'typeorm';
-import { validate } from "uuid";
+import { User } from 'src/modules/users/entities/User';
+import connection from '../../../infra/typeorm';
 import { UsersRepository } from "../../../modules/users/repositories/implementations/UsersRepository";
 
-describe("Users Repository", () => {
-    let connection: Connection;
+let usersRepository: UsersRepository;
 
-    let ormUsersRepository: Repository<User>;
-
-    beforeAll(async () => {
-        connection = await createConnection();
-
-        ormUsersRepository = getRepository(User);
-    })
-
-    it("should be able to create a new user", async () => {
-        const user = await usersRepository.create({
-            name: 'John Doe',
-            email: 'johndoe@email.com',
-            password: 'password',
-        });
-
-        expect(user).toMatchObject({
-            name: 'John Doe',
-            email: 'johndoe@email.com',
-            password: 'password',
-            avatar: 'johndoeavatar',
-        });
+describe("Repository | User", () => {
+    beforeAll(async ()=>{
+        await connection.create();
+        usersRepository = new UsersRepository();
+    });
+      
+    afterAll(async ()=> {
+        await connection.close();
+    });
+    
+    beforeEach(async () => {
+        await connection.clear();
     });
 
-    // it("should be able to list all users", () => {
-    //     const user = usersRepository.create({
-    //         name: 'John Doe',
-    //         email: 'johndoe@email.com',
-    //         password: 'password',
-    //         avatar: 'johndoeavatar',
-    //     });
+    it("should be able to create a new user", async () => {
+        const user = {
+            name: 'John Doe',
+            email: 'john@email.com',
+            password: 'john',
+            created_at: new Date(),
+            updated_at: new Date(),
+        };
 
-    //     const users = usersRepository.list();
+        await usersRepository.create(user);
 
-    //     expect(users).toStrictEqual(expect.arrayContaining([user]));
-    // });
+        const users = await usersRepository.list();
+
+        expect(users).toHaveLength(1);
+    });
+
+    it("should be able to list all users", async () => {
+        const firstUser: User = {
+            name: 'John Doe',
+            email: 'john@email.com',
+            password: 'john',
+            created_at: new Date(),
+            updated_at: new Date()
+        };
+
+        const secondUser: User = {
+            name: 'Ana Linz',
+            email: 'ana@email.com',
+            password: 'ana',
+            created_at: new Date(),
+            updated_at: new Date()
+        };
+
+        await usersRepository.create(firstUser);        
+        await usersRepository.create(secondUser);
+
+        expect(await usersRepository.list()).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({email: 'john@email.com'}),
+              expect.objectContaining({email: 'ana@email.com'})
+            ])
+          );
+    });
+
+    it("should be able find an user with the findByEmail method", async () => {
+        const user: User = {
+            name: 'John Doe',
+            email: 'john@email.com',
+            password: 'john',
+            created_at: new Date(),
+            updated_at: new Date()
+        };
+
+        await usersRepository.create(user);        
+
+        const userFound = await usersRepository.findByEmail(user.email);
+
+        console.log(user);
+
+        expect(userFound).toEqual(user);
+    });
 });
