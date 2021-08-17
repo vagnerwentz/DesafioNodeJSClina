c71061490276095fcc0875a4e251961cc699a7ad
Vagner Wentz <wentz.vagner@gmail.com>
2021-08-16 23:59:30 -0300

    feat: Add a method to find by email.

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,15 +1,15 @@
-import { User } from '../model/User';
+import { User } from '../entities/User';
 
 interface ICreateUserDTO {
     name: string;
     email: string;
     password: string;
-    avatar: string;
 }
 
 interface IUsersRepository {
-    create({ name, email, password, avatar }: ICreateUserDTO): Promise<User>;
+    create({ name, email, password }: ICreateUserDTO): Promise<void>;
     list(): Promise<User[]>;
+    findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository, ICreateUserDTO };
