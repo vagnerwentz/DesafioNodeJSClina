9c6fb4dff824b772265c4d968f1e13011e060109
Vagner Wentz <vagner.wentz@objective.com.br>
2021-08-17 09:54:56 -0300

    feat: Add a constructor to initialize a uuid.

--- src/modules/users/entities/User.ts
+++ src/modules/users/entities/User.ts
@@ -1,8 +1,9 @@
-import { Column, Entity, CreateDateColumn, UpdateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, CreateDateColumn, UpdateDateColumn, PrimaryColumn } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
 
 @Entity('users')
 class User {
-    @PrimaryGeneratedColumn('uuid')
+    @PrimaryColumn()
     id: string;
 
     @Column()
@@ -19,6 +20,12 @@ class User {
 
     @UpdateDateColumn()
     updated_at: Date;
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuidV4();
+        }
+    }
 }
 
 export { User };
